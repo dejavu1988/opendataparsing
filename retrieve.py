224a4f2892fdd4f236b6bf32274fa1d4a4b488c8
dejavu1988 <rekygx@gmail.com>
2013-12-08 21:56:14 +0200

    trunk done with one entry incomplete to be fixed

--- retrieve.py
+++ retrieve.py
@@ -36,7 +36,7 @@ def extract(data):
     if archive.testzip() != None:
         print "Invalid zipfile"
         sys.exit() 
-    record = []
+    recordlist = []
     for name in archive.namelist():
         print "Processing file: " + name
         f = archive.open(name, 'rU')
@@ -44,16 +44,68 @@ def extract(data):
         while tmpstr:
             if tmpstr[0] != '#':
                 entry = tmpstr[:-1].split('|')
-                record.append(entry)
+                if len(entry) != 9:
+                    print entry
+                recordlist.append(tuple(entry))
             tmpstr = f.readline()
         f.close()
     archive.close()
     data.close()
-    print "Extracted to record list: " + str(len(record)) + " entries."
-    return record
+    print "Extracted to record list: " + str(len(recordlist)) + " entries."
+    return recordlist
 
+def dump(recordlist):
+    f = open('record.csv','w')
+    for entry in recordlist:
+        tmpstr = ''
+        for item in entry:
+            tmpstr += item + '|'
+        f.write(tmpstr[:-1]+'\n')
+    f.close()
+    print "Record object dumped."
+
+def analyseMainCategory(recordlist):
+    print "Analysing MainCategory-Frequency.."
+    catedict = {}
+    for entry in recordlist:
+        if len(entry) != 9:
+            print entry
+            continue
+        cate = entry[5]
+        if catedict.has_key(cate):
+            catedict[cate] = catedict[cate] + 1
+        else:
+            catedict[cate] = 1
+    return catedict
+
+def analyseLocation(recordlist):
+    print "Analysing Location-Frequency.."
+    locdict = {}
+    for entry in recordlist:
+        if len(entry) != 9:
+            print entry
+            continue
+        loc = entry[7]+entry[8]
+        if locdict.has_key(loc):
+            locdict[loc] = locdict[loc] + 1
+        else:
+            locdict[loc] = 1
+    return locdict
+
+def output(resdict, path):
+    f = open(path, 'w')
+    for key in resdict.keys():
+        entry = key+' '+str(resdict[key])+'\n'
+        print entry
+        f.write(entry)
+    f.close()
 
 if __name__ == "__main__":
     remotePath = "http://pilvilinna.cert.fi/opendata/autoreporter/csv.zip"
     data = retrieve(remotePath)
-    record = extract(data)
+    recordlist = extract(data)
+    dump(recordlist)
+    catedict = analyseMainCategory(recordlist)
+    locdict = analyseLocation(recordlist)
+    output(catedict, 'category-frequency.csv')
+    output(locdict, 'location-frequency.csv')
